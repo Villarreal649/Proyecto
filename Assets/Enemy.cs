ee8f11b1f9e3220205f02018a4e97b200313517f
alansinbailarin <alan_y_perla@hotmail.com>
2023-03-03 21:13:52 -0800

    Se añadio el spawn de los zombies y la muerte del personaje

---
+++ Assets/Enemy.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 3f;
+    public float rotateSpeed = 0.0025f;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (!target)
+        {
+            GetTarget();
+        }
+        else
+        {
+            RotateTowardsTarget();
+        }
+
+    }
+
+    private void RotateTowardsTarget()
+    {
+        Vector2 targetDirection = target.position - transform.position;
+        float angle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg - 90f;
+
+        Quaternion q = Quaternion.Euler(new Vector3(0, 0, angle));
+
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, q, rotateSpeed);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = transform.up * speed;
+    }
+
+    private void GetTarget()
+    {
+        if (GameObject.FindGameObjectWithTag("Player"))
+        {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+        Destroy(other.gameObject);
+        target = null;
+        }
+        else if (other.gameObject.CompareTag("Bullet"))
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+}
